53aec7746adffc727077807a95da12f82f7e08b1
Al Fair <thealisonfair@gmail.com>
2017-09-19 13:28:10 -0400

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.create(
-#   username: "oofstar",
-#   team_name: "Firth Street Cutie Crew",
-#   password: "password",
-# )
-#
-# Cuty.create(
-#   name: "Alox",
-#   cutie_pic: "http://oofindustries.net/images/aloxpic.jpg",
-#   bio: "Alox is a well known QTV personality and the mastermind behind the concept of Spoon Therapy.",
-#   cutie_type: "Softie",
-#   species: "Polar Bear",
-#   birthdate: 19851008,
-#   user_id: User.first.id
-# )
-#
-# 4.times do
-#   Post.create(
-#     body: Faker::Lovecraft.paragraph,
-#     cuty_id: Cuty.first.id
-#   )
-# end
+User.create(
+  username: "oofstar",
+  team_name: "Firth Street Cutie Crew",
+  first_name: "Al",
+  last_name: "Fair",
+  email: "geek.sheek@gmail.com",
+  password: "password",
+)
+
+Cuty.create(
+  name: "Alox",
+  cutie_pic: "http://oofindustries.net/images/aloxpic.jpg",
+  bio: "Alox is a well known QTV personality and the mastermind behind the concept of Spoon Therapy.",
+  cutie_type: "Softie",
+  species: "Polar Bear",
+  birthdate: 19851008,
+  user_id: User.last.id
+)
+
+4.times do
+  Post.create(
+    body: Faker::Lovecraft.paragraph,
+    cuty_id: Cuty.last.id
+  )
+end
