06f984d1d629d35352cbbbbfd33f60c450723aed
Al Fair <thealisonfair@gmail.com>
2017-09-20 12:29:58 -0400

    uploading works locally

--- app/controllers/cuties_controller.rb
+++ app/controllers/cuties_controller.rb
@@ -15,4 +15,22 @@ class CutiesController < ApplicationController
     @cutie = Cuty.new
   end
 
+  def create
+    @cutie =Cuty.new(cuty_params)
+    @cutie.user = current_user
+
+    if @cutie.save
+      redirect_to @cutie, notice: "Cutie Successfully Added!"
+    else
+      flash[:notice] = @cutie.errors.full_messages.join(', ')
+      render action: 'new'
+    end
+  end
+
+  private
+
+  def cuty_params
+    params.require(:cuty).permit(:name, :bio, :cutie_type, :species, :birthdate, :cutie_pic)
+  end
+
 end
--- app/models/cuty.rb
+++ app/models/cuty.rb
@@ -1,4 +1,6 @@
 class Cuty < ApplicationRecord
+  mount_uploader :cutie_pic, CutiePicUploader
+
   belongs_to :user
   has_many :posts
 
