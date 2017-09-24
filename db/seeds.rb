511d848f265652d44b14f852bd36f918dd1acf8b
Al Fair <thealisonfair@gmail.com>
2017-09-24 14:31:33 -0400

    successfully seeded on guy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,43 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-10.times do
-  User.create(
-    username: Faker::Internet.user_name,
-    team_name: Faker::Space.agency,
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
-    password: "password"
+User.create(
+  username: "Oofstar",
+  team_name: "Firth Street Cutie Crew",
+  first_name: "Al",
+  last_name: "Fair",
+  email: "geek.sheek@gmail.com",
+  password: "password"
+)
+
+user_1 = User.first
+
+my_cuties = [
+
+  [
+    "Alex P Fair",
+    Rails.root.join('public', 'images', 'alex.jpg' ).open,
+    "Alex grew up in brooklyn and misses it terribly!",
+    "Softie",
+    "Polar Bear",
+    19851007,
+    user_1
+  ]
+
+]
+
+my_cuties.each do |cutie_info|
+  name, cutie_pic, bio, cutie_type, species, birthdate, user = cutie_info
+  Cuty.create(
+    name: name,
+    cutie_pic: cutie_pic,
+    bio: bio,
+    cutie_type: cutie_type,
+    species: species,
+    birthdate: birthdate,
+    user: user
   )
-end
-
-users = User.all
-users.each do |user|
-  3.times do
-    Cuty.create(
-      name: Faker::Lovecraft.deity,
-      cutie_pic: Rails.root.join('spec', 'support', 'images', 'photo.jpg' ).open,
-      bio: Faker::Lovecraft.paragraph,
-      cutie_type: ["Softie", "Hard Guy", "Action Figure"].sample,
-      species: Faker::HitchhikersGuideToTheGalaxy.specie,
-      birthdate: Faker::Date.birthday(18, 65),
-      user: user
-    )
-  end
-end
-
-cuties = Cuty.all
-
-cuties.each do |cutie|
-  4.times do
-    Post.create(
-      body: Faker::Lovecraft.paragraph,
-      cuty: cutie
-    )
-  end
 end
